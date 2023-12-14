abc58888443444a246c40c2419c1248a469fce16
tvelic1 <tvelic1@etf.unsa.ba>
2023-12-15 00:56:01 +0100

    exclude views

--- Implementacija/Implementacija/Startup.cs
+++ Implementacija/Implementacija/Startup.cs
@@ -1,3 +1,4 @@
+
 using Implementacija.Data;
 using Implementacija.Models;
 using Implementacija.Services;
@@ -12,12 +13,16 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
 namespace Implementacija
 {
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
